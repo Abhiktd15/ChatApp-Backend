f372664008c22c2400f47f1903dc597274614a6a
Abhishek Bhardwaj <ayushindia4@gmail.com>
2024-09-17 19:09:40 +0530

    Created getMyChat Api

--- controllers/chat.controller.js
+++ controllers/chat.controller.js
@@ -1,4 +1,5 @@
 import { ALERT, REFETCH_CHATS } from "../constants/events.js";
+import { getOtherMember } from "../lib/helper.js";
 import { TryCatch } from "../middlewares/error.js"
 import {Chat} from '../models/chat.model.js'
 import { emitEvent } from "../utils/features.js";
@@ -25,4 +26,31 @@ const newGroupChat = TryCatch(async (req,res,next) => {
     })
 })
 
-export {newGroupChat}
+const getMyChats = TryCatch(async (req,res,next) => {
+
+    const chats = await Chat.find({members:req.user}).populate(
+        "members",
+        "name avatar"
+    );
+    const transformedChats = chats.map(({ _id, name, members, groupChat }) => {
+        const otherMember = getOtherMember(members, req.user);
+    
+        return {
+            _id,
+            groupChat,
+            avatar: groupChat
+                ? members.slice(0, 3).map(({ avatar }) => avatar.url)
+                : [otherMember.avatar.url],
+            name: groupChat ? name : otherMember.name,
+            members: members
+        }
+    })
+    
+    
+    return res.status(200).json({
+        success: true,
+        chats: transformedChats
+    });
+});
+
+export {newGroupChat,getMyChats}
--- routes/chat.routes.js
+++ routes/chat.routes.js
@@ -1,6 +1,6 @@
 import express from "express";
 import { isAuthenticated } from "../middlewares/auth.js";
-import { newGroupChat } from "../controllers/chat.controller.js";
+import { getMyChats, newGroupChat } from "../controllers/chat.controller.js";
 
 const app = express.Router();
 
@@ -8,5 +8,6 @@ const app = express.Router();
 app.use(isAuthenticated);//This ensures below this line every route isAuthenticated 
 
 app.post('/new',newGroupChat);
+app.get('/my',getMyChats);
 
 export default app;
